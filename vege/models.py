1d7bae7abc0c7a59cc5cdcea1f36f3d660c94bfd
rir7890 <Rahul Kumar>
2024-04-16 20:16:43 +0530

    last update

--- vege/models.py
+++ vege/models.py
@@ -8,3 +8,4 @@ class Recipe(models.Model):
     recipe_name = models.CharField(max_length=50)
     recipe_description = models.TextField()
     image_field = models.ImageField(upload_to='recipe/')
+    recipe_view_count = models.IntegerField(default=1)
